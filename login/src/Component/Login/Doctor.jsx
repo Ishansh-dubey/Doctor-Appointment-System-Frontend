48bd0f1310e246b869b1ba7bb8c4c4f8efe9432b
Ishansh <ishanshdubey3@gmail.com>
2024-03-21 10:21:04 +0530

    added upload and download pdf feature

--- login/src/Component/Login/Doctor.jsx
+++ login/src/Component/Login/Doctor.jsx
@@ -30,19 +30,60 @@
 //   );
 // }
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAuth } from "./Securirty/AuthContext";
 import { Navigate, useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import axios from 'axios';
 import BookingComponent from "./Booking";
+
 
 export default function DoctorComponent() {
   const authContext = useAuth();
   const { user_id } = useParams();
   const [bookings, setBookings] = useState([]);
   const [pastBookings, setPastBookings] = useState([]);
+  const [uploadStatus, setUploadStatus] = useState({});
+ const [uploadParams, setUploadParams] = useState({});
+  const [uploadStatusMap, setUploadStatusMap] = useState({});
+   const [error, setError] = useState(null);
+   const [selectedOption, setSelectedOption] = useState({});
+  const fileInputRef = useRef(null);
 
   const navigate = useNavigate();
+
+//   // Define a function to manually set the option to "right"
+// const setOptionRight = (booking_id) => {
+//   // Update the option to "right" for the corresponding booking
+//   // This is just a placeholder, you need to implement the actual logic to update the option in your backend
+//   console.log(`Manually setting option to "right" for booking ID ${booking_id}`);
+// };
+
+// // Define a function to manually set the option to "wrong"
+// const setOptionWrong = (booking_id) => {
+//   // Update the option to "wrong" for the corresponding booking
+//   // This is just a placeholder, you need to implement the actual logic to update the option in your backend
+//   console.log(`Manually setting option to "wrong" for booking ID ${booking_id}`);
+// };
+
+ // Function to mark the option as right
+  const markOption = (bookingId, option) => {
+    // Update the selected option for the booking
+    setSelectedOption((prevOptions) => {
+      const updatedOptions = { ...prevOptions, [bookingId]: option };
+      // Store the selected option in local storage
+      localStorage.setItem("selectedOptions", JSON.stringify(updatedOptions));
+      return updatedOptions;
+    });
+  };
+
+  useEffect(() => {
+    // Retrieve selected options from local storage when component mounts
+    const storedOptions = JSON.parse(localStorage.getItem("selectedOptions"));
+    if (storedOptions) {
+      setSelectedOption(storedOptions);
+    }
+  }, []);
 
   // useEffect ( () =>{
   //   fetchBookings();
@@ -185,7 +226,7 @@ export default function DoctorComponent() {
           return bookingDate < today;
         });console.log(filteredBookings);
 
-        //sort filtered bookings by time in ascending order
+        //sort filtered bookings by time in dscending order
         const pastBookings = filteredBookings.sort((a,b) => {
         const timeA = new Date(`${a.booking_date}T${a.booking_time}`);
         const timeB = new Date(`${b.booking_date}T${b.booking_time}`);
@@ -209,30 +250,272 @@ export default function DoctorComponent() {
   }, [user_id, authContext.user]);
   
 
-  // Function to handle report submission
-  async function handleReportSubmission(patientId, reportData) {
+  //Function to upload file to Dropbox and get shared link
+  // async function uploadFileToDropbox(){
+  //   try{
+  //     const dbx = new Dropbox({
+  //       accessToken: ""
+  //     });
+  //     const response = await dbx.filesUpload({})
+  //     const sharedLinkResponse = await dbx.sharingCreateSharedLinkWithSettings({path});
+  //     const sharedLink = sharedLinkResponse.result;
+  //     savePdfLinkToDatabase(sharedLink);
+  //   }catch (error){
+  //     console.error("Error uploading file to Dropbox:", error);
+  //   }
+  // }
+
+  // async function savePdfLinkToDatabase(link) {
+  //   try {
+  //     const response = await fetch("/savePdfLink", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json"
+  //       },
+  //       body: JSON.stringify({ link })
+  //     });
+  //     const data = await response.json();
+  //     if (response.ok) {
+  //       setPdfLink(data.link);
+  //     } else {
+  //       console.error("Failed to save PDF link:", data.message);
+  //     }
+  //   } catch (error) {
+  //     console.error("Error saving PDF link to the database:", error);
+  //   }
+  // }
+
+  //Function to handle report submission
+//    async function handleReportSubmission(booking_id, patient_name, booking_date) {
+//   try {
+//     // Programmatically trigger file input click
+//     fileInputRef.current.click();
+//     setUploadStatus(null); // Reset upload status
+//     setUploadParams({ booking_id, patient_name, booking_date }); // Store parameters for file upload
+//   } catch (error) {
+//     console.error("Error handling report submission:", error);
+//   }
+// }
+
+// async function handleFileUpload(e) {
+//   const { booking_id, patient_name, booking_date } = uploadParams;
+//   if (!booking_id || !patient_name || !booking_date) {
+//     console.error("Booking details not available for file upload");
+//     return;
+//   }
+
+//   const file = e.target.files[0];
+//   if (!file) return;
+
+//   const formData = new FormData();
+//   formData.append("report_id", booking_id);
+//   formData.append("patient_report_name", patient_name);
+//   formData.append("doctor_report_id", authContext.user.doctor_id);
+//   formData.append("booking_date", booking_date);
+//   formData.append("pdf", file);
+
+//   try {console.log(formData);
+//     const response = await fetch("http://localhost:8080/uploadReport", {
+//       method: "POST",
+//       body: formData,
+//     });
+
+//     if (response.ok) {
+//       setUploadStatus("success");
+//     } else {
+//       setUploadStatus("failure");
+//       console.error("Failed to upload report");
+//     }
+//   } catch (error) {
+//     console.error("Error uploading report:", error);
+//     setUploadStatus("failure");
+//   }
+// }
+
+// async function handleReportSubmission(booking_id, patient_name, booking_date) {
+//     try {
+//       fileInputRef.current.click();
+//       setUploadStatus(prevStatus => ({ ...prevStatus, [booking_id]: null }));
+//       setUploadParams({ booking_id, patient_name, booking_date });
+//     } catch (error) {
+//       console.error("Error handling report submission:", error);
+//     }
+//   }
+
+//   async function handleFileUpload(e) {
+//     const { booking_id, patient_name, booking_date } = uploadParams;
+//     if (!booking_id || !patient_name || !booking_date) {
+//       console.error("Booking details not available for file upload");
+//       return;
+//     }
+
+//     const file = e.target.files[0];
+//     if (!file) return;
+
+//     const formData = new FormData();
+//     formData.append("booking_report_id", booking_id);
+//     formData.append("patient_report_name", patient_name);
+//     formData.append("doctor_report_id", authContext.user.doctor_id);
+//     formData.append("booking_date", booking_date);
+//     formData.append("pdf", file);
+
+//     try {
+//       const response = await fetch("http://localhost:8080/uploadReport", {
+//         method: "POST",
+//         body: formData,
+//       });
+
+//       const responseData = await response.json(); // Parse response data as JSON
+//       console.log(responseData);
+//       if (response.ok) {
+//         setUploadStatus(prevStatus => ({ ...prevStatus, [booking_id]: "success" }));
+//         setUploadStatusMap(responseData);
+//       } else {
+//         setUploadStatus(prevStatus => ({ ...prevStatus, [booking_id]: "failure" }));
+//         console.error("Failed to upload report");
+//       }
+//     } catch (error) {
+//       console.error("Error uploading report:", error);
+//       setUploadStatus(prevStatus => ({ ...prevStatus, [booking_id]: "failure" }));
+//     }
+//   }
+
+// Function to save report ID in local storage
+const saveReportIdToLocalStorage = (bookingId, reportId) => {
+    const storageKey = `report_${bookingId}`;
+    localStorage.setItem(storageKey, reportId);
+};
+
+// Function to retrieve report ID from local storage
+const getReportIdFromLocalStorage = (bookingId) => {
+    const storageKey = `report_${bookingId}`;
+    return localStorage.getItem(storageKey);
+};
+
+async function handleReportSubmission(booking_id, patient_name, booking_date) {
     try {
-      const response = await fetch(`/uploadReport/${patientId}`, {
+      fileInputRef.current.click();
+      setUploadStatus(prevStatus => ({ ...prevStatus, [booking_id]: null }));
+      setUploadParams({ booking_id, patient_name, booking_date });
+    } catch (error) {
+      console.error("Error handling report submission:", error);
+    }
+}
+
+async function handleFileUpload(e) {
+    const { booking_id, patient_name, booking_date } = uploadParams;
+    if (!booking_id || !patient_name || !booking_date) {
+      console.error("Booking details not available for file upload");
+      return;
+    }
+
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("booking_report_id", booking_id);
+    formData.append("patient_report_name", patient_name);
+    formData.append("doctor_report_id", authContext.user.doctor_id);
+    formData.append("booking_date", booking_date);
+    formData.append("pdf", file);
+
+    try {
+      const response = await fetch("http://localhost:8080/uploadReport", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(reportData),
+        body: formData,
       });
 
-      if (!response.ok) {
-        throw new Error("Failed to upload report");
+      const responseData = await response.json(); // Parse response data as JSON
+      console.log(responseData);
+      if (response.ok) {
+        setUploadStatus(prevStatus => ({ ...prevStatus, [booking_id]: "success" }));
+        setUploadStatusMap(prevMap => ({ ...prevMap, [booking_id]: responseData.report_id }));
+         saveReportIdToLocalStorage(booking_id, responseData.report_id); // Save report ID in local storage
+      } else {
+        setUploadStatus(prevStatus => ({ ...prevStatus, [booking_id]: "failure" }));
+        console.error("Failed to upload report");
       }
-
-      // Update UI or show success message
     } catch (error) {
       console.error("Error uploading report:", error);
+      setUploadStatus(prevStatus => ({ ...prevStatus, [booking_id]: "failure" }));
     }
-  }
+}
+
+
+  // async function handleDownloadPDF(bookingId) {
+  //   try {
+  //     const response = await fetch(`http://localhost:8080/downloadReport?booking_report_id=${bookingId}`, {
+  //       method: 'GET',
+  //     });
+
+  //     if (!response.ok) {
+  //       throw new Error('Failed to download PDF');
+  //     }
+
+  //     // Create a Blob from the PDF response
+  //     const blob = await response.blob();
+
+  //     // Create a temporary URL for the Blob object
+  //     const url = window.URL.createObjectURL(blob);
+
+  //     // Create a temporary link element
+  //     const link = document.createElement('a');
+  //     link.href = url;
+  //     link.setAttribute('download', `report_${bookingId}.pdf`);
+      
+  //     // Append the link to the document body and programmatically click it
+  //     document.body.appendChild(link);
+  //     link.click();
+
+  //     // Remove the temporary link
+  //     document.body.removeChild(link);
+  //   } catch (error) {
+  //     console.error('Error downloading PDF:', error);
+  //   }
+  // }
+
+  const handleDownloadPDF = async (report_id) => {
+    try {console.log(report_id);
+      // Make a GET request to the API endpoint
+      const response = await axios.get(`http://localhost:8080/downloadReport`, {
+        params: {
+           report_id// Replace with actual report ID
+        },
+        responseType: 'blob' // Receive response as binary data
+      });
+
+      // Create a Blob object from the binary data
+      const blob = new Blob([response.data], { type: 'application/pdf' });
+
+      // Create a URL for the Blob
+      const url = window.URL.createObjectURL(blob);
+
+      // Create a temporary link element to trigger the download
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = 
+      document.body.appendChild(link);
+
+      // Trigger the download
+      link.click();
+
+      // Clean up
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(link);
+    } catch (error) {
+      setError('Failed to download report');
+      console.error('Error downloading report:', error);
+    }
+  };
+
+  // const APP_KEY = "rpbatmbf1lx0l0m";
 
    function handleSignout(){
     navigate("/signout")
    }
+
+   //upload Report
+
 
   // Redirect to home if user is not authenticated or is not a doctor
   if (!authContext.isAuthenticated || !authContext.doctor) {
@@ -269,18 +552,68 @@ export default function DoctorComponent() {
               <th>Date</th>
               <th>Time</th>
               <th>Patient Name</th>
+              <th>Report ID</th>
               <th>Actions</th>
+              <th>Upload Status</th>
+              <th>Status</th>
             </tr>
           </thead>
           <tbody>
+
             {bookings.map((booking) => (
               <tr key={booking.booking_id}>
               <td>{booking.booking_id}</td>
                 <td>{booking.booking_date}</td>
                 <td>{booking.booking_time}</td>
                 <td>{booking.patient_name}</td>
+                <td>{getReportIdFromLocalStorage(booking.booking_id)}</td>
+                
                 <td>
+                 <button
+                    onClick={() =>
+                      handleReportSubmission(
+                        booking.booking_id,
+                        booking.patient_name,
+                        booking.booking_date
+                      )
+                    }
+                  >
+                    Upload Report
+                  </button>
+                  <input
+                    type="file"
+                    ref={fileInputRef}
+                    style={{ display: "none" }}
+                    onChange={handleFileUpload}
+                  />
+                  
 
+                   <button onClick={() => {
+  const report_id = getReportIdFromLocalStorage(booking.booking_id);
+  handleDownloadPDF(report_id);
+}}>Download PDF</button>
+
+                </td>
+                <td>
+                  {uploadStatus[booking.booking_id] === "success" && (
+                    <p>Report uploaded successfully. </p>
+                  )}
+                  {uploadStatus[booking.booking_id] === "failure" && (
+                    <p>Failed to upload report</p>
+                  )}</td>
+                    <td>
+                  {selectedOption[booking.booking_id] ? (
+                    selectedOption[booking.booking_id] === "right" ? (
+                      <span style={{ color: "green" }}>&#10004;</span>
+                    ) : (
+                      <span style={{ color: "red" }}>&#10006;</span>
+                    )
+                  ) : (
+                    <>
+                      <button onClick={() => markOption(booking.booking_id, 'right')}> <span style={{ color: "green" }}>&#10004; </span></button>
+                      <button onClick={() => markOption(booking.booking_id, 'wrong')}> <span style={{ color: "red" }}>&#10006; </span></button>
+                    </>
+                  )}
                 </td>
               </tr>
             ))}
@@ -310,9 +643,10 @@ export default function DoctorComponent() {
                 <td>{bookings.booking_time}</td>
                 <td>{bookings.patient_name}</td>
                 <td>
-                  <button >
-                      Upload Report
-                    </button>
+                  <button onClick={() => {
+  const report_id = getReportIdFromLocalStorage(bookings.booking_id);
+  handleDownloadPDF(report_id);
+}}>Download PDF</button>
                 </td>
               </tr>
             ))}
